1db60e15a755f3b61eba83d8db5c4f83cdc5c228
AndreyMota <andreymotam1@gmail.com>
2023-07-11 02:33:50 -0300

    login e cadastro

--- src/controllers/authcController.js
+++ src/controllers/authcController.js
@@ -1,4 +1,6 @@
 import { db } from "../database.js";
+import bcrypt from 'bcrypt';
+import {v4 as uuid} from 'uuid';
 
 export async function signUp(req, res) {
   const { email, password } = req.body;
@@ -9,8 +11,28 @@ export async function signUp(req, res) {
     res.status(409).json({ error: "Email already registered" });
     return;
   } else {
-    db.collection('users').insertOne({ email: email, password: password });
+    const senhaCrip = bcrypt.hashSync(password, 4);
+    db.collection('users').insertOne({ email: email, password: senhaCrip });
     res.status(201).send('success');
   }
 }
   
+export async function signIn(req, res) {
+  const { email, password } = req.body;
+
+  const existingUser = await db.collection('users').findOne({ email: email });
+
+  if (existingUser) {
+    const bate = bcrypt.compareSync(password, existingUser.password);
+    if (bate) {
+      const token = uuid();
+      await db.collection("sessions").insertOne({userId: existingUser._id,token});
+      res.status(200).json({ token: token})
+    } else {
+      res.status(401).send('Senha incorreta');
+    }
+  } else {
+    res.status(404).send('Email não cadastrado.');
+  }
+
+}
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signUp } from "../controllers/authcController.js";
+import { signUp, signIn } from "../controllers/authcController.js";
 import { validaBodyAuth } from "../schemas/authSchema.js";
 
 const router = Router();
 router.post('/cadastro', validaBodyAuth, signUp);
+router.post('/', validaBodyAuth, signIn);
 
 export default router;
